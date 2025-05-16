b307f5668926ad6e26a4790789e99ea2e0484811
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 12:52:16 +0000

    追加

--- docs/homepage.js
+++ docs/homepage.js
@@ -1,13 +1,10 @@
+for (let i = 1; i <= 3; i++) {
+  const button = document.getElementById(`issueButton${i}`);
+  const targetId = `target${i}`;
 
-const issueButton = [];
-const link = [];
-for (let i = 1; i <=3; i++){
-  issueButton[i] = document.getElementById(`issueButton${i}`);
-  link[i] = document.getElementById(`link${i}`);
-
-  if (issueButton[i] && link[i]) {
-    issueButton[i].addEventListener("click", () => {
-      link[i].click();
+  if (button) {
+    button.addEventListener("click", () => {
+      location.hash = `#${targetId}`;
     });
   }
-};
+}
