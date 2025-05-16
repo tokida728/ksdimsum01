1ac0bc6423231c1e77caf49942b2b126a8a5bbbe
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 12:14:27 +0000

    追加

--- docs/homepage.js
+++ docs/homepage.js
@@ -5,7 +5,14 @@ for (let i = 1; i <=3; i++){
   issueButton[i] = document.getElementById(`issueButton${i}`);
   target[i] = document.getElementById(`target${i}`);
 
-  issueButton[i].addEventListener("click", () => {
-    target[i].scrollIntoView();
-  });
+  if (issueButton[i] && target[i]) {
+    issueButton[i].addEventListener("click", () => {
+      target[i].scrollIntoView();
+      target[i].style.transition = "background 0.3s";
+      target[i].style.background = "yellow";
+      setTimeout(() => {
+        target[i].style.background = "lightgray";
+      }, 600);
+    });
+  }
 };
