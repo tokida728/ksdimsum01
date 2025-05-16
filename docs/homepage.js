7b67ef2e13a0b945d7e33e2120228a0c4a2761b6
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 11:42:11 +0000

    追加

--- docs/homepage.js
+++ docs/homepage.js
@@ -1,15 +1,12 @@
 
 const issueButton = [];
-for (let i = 1; i <=3; i++){
-  issueButton[i] = document.getElementById("issueButton[i]");
-};
-
 const target = [];
 for (let i = 1; i <=3; i++){
-  issueButton[i] = document.getElementById("target[i]]");
+  issueButton[i] = document.getElementById(`issueButton${i}`);
+  issueButton[i] = document.getElementById(`target${i}`);
 };
 
 
-issueButton.addEventListener("click", () => {
-  target.scrollIntoView({ behavior: "smooth" });
+issueButton[i].addEventListener("click", () => {
+  target[i].scrollIntoView({ behavior: "smooth" });
 });
