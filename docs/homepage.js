08bf1817c3e78c2c9668fdc4794c28903a3c5b84
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 10:54:46 +0000

    スクロールボタンとターゲット修正

--- docs/homepage.js
+++ docs/homepage.js
@@ -1,6 +1,6 @@
-const issueButton01 = document.getElementById(`issueButton01`);
-const target01 = document.getElementById(`target01`);
+const issueButton01 = document.getElementById("issueButton01");
+const target01 = document.getElementById("target01");
 
-issueButton01.addEventListener("click", () => { 
-target01.scrollIntoView({ behaivor: "smooth"})
+issueButton01.addEventListener("click", () => {
+  target01.scrollIntoView({ behavior: "smooth" });
 });
