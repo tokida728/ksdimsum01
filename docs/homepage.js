3ed175b51d7b463cc3fb8bfcb25d9b4c403917f5
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 11:49:23 +0000

    追加

--- docs/homepage.js
+++ docs/homepage.js
@@ -3,7 +3,7 @@ const issueButton = [];
 const target = [];
 for (let i = 1; i <=3; i++){
   issueButton[i] = document.getElementById(`issueButton${i}`);
-  issueButton[i] = document.getElementById(`target${i}`);
+  target[i] = document.getElementById(`target${i}`);
 
   issueButton[i].addEventListener("click", () => {
     target[i].scrollIntoView({ behavior: "smooth" });
