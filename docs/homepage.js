55379cc7d83fff887f172a2a5a82609999e396e3
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-16 10:08:46 +0000

    ファイル追加

---
+++ docs/homepage.js
@@ -0,0 +1,6 @@
+const issueButton01 = document.getElementById(`issueButton01`);
+const target01 = getElementById(`target01`);
+
+issueButton01.addEventListener("click", () => { 
+target01.scrollIntoView({ behaivor: "smooth"})
+});
