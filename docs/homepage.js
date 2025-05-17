d43c60ea8ab5a5a5fe3faf5b67aee653e8ff8e84
tokida728 <65758695+tokida728@users.noreply.github.com>
2025-05-17 16:05:32 +0000

    追加

--- docs/homepage.js
+++ docs/homepage.js
@@ -1,10 +1,13 @@
-for (let i = 1; i <= 3; i++) {
-  const button = document.getElementById(`issueButton${i}`);
-  const targetId = `target${i}`;
+// ページの要素が読み込まれてからイベント登録
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 1; i <= 3; i++) {
+    const button = document.getElementById(`issueButton${i}`);
+    const targetId = `target${i}`;
 
-  if (button) {
-    button.addEventListener("click", () => {
-      location.hash = `#${targetId}`;
-    });
+    if (button) {
+      button.addEventListener("click", () => {
+        location.hash = `#${targetId}`;
+      });
+    }
   }
-}
+});
